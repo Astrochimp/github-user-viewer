92b8fc9d439f6bcb703c2ce5e0a8891287a1ec14
Marcello Prattico <chimp@astrochimp.com>
2023-08-21 14:57:00 -0400

    add basic query

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [repos, setRepos] = useState<[]>([]);
+  const user = 'Astrochimp';
+
+  useEffect(() => {
+    async function getUser() {
+      const res = await fetch(`https://api.github.com/users/${user}/repos?page=1&per_page=20&sort=updated`);
+      const data = await res.json();
+      setRepos(data);
+    }
+
+    getUser();
+  }, []);
 
   return (
-    <>
+    <div>
+      <h1>{user} Repos</h1>
+      <div className=''>
+        {repos.map((rep) => {
+          console.log('rep', rep);
+          return (
+            <>
+              <h2>{rep.full_name}</h2>
+              <p>
+                <a href={rep.html_url} target="_blank">{rep.html_url}</a>
+              </p>
+              <div>{rep.language}</div>
+              <div>⭐️{rep.stargazers_count}</div>
+            </>
+          );
+        })}
+      </div>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
