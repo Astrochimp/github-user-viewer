704392b16ad81c803f9b5262138fd767d9641b81
Marcello Prattico <chimp@astrochimp.com>
2023-08-21 21:41:27 -0400

    add one test

--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -5,7 +5,7 @@ import App from "../src/App";
 describe('App', () => {
   it('renders headline', () => {
     render(<App />);
-    const headline = screen.getByText(/It works and you found me!/i);
-    expect(headline).toBeInTheDocument();
+    const wrapper = screen.getByTestId('app-wrapper');
+    expect(wrapper).toBeInTheDocument();
   });
 });
